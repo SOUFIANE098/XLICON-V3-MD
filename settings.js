23700463231aa0620dc7488273454f9ae2be6b90
Botwharsapp <103326219+Botwharsapp@users.noreply.github.com>
2024-03-12 03:16:29 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -58,7 +58,7 @@ global.ownername = process.env.SUDO_NAME || 'Salman Ahmad' //ur owner name
 //________________________________________________________________________________________________________
 global.websitex = process.env.YT_LINK || "https://youtube.com/@s4salmanyt"
 //________________________________________________________________________________________________________
-global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
+global.wagc = process.env.GL_PUSH || "https://chat.whatsapp.com/L8pXXpk4qecAKZFycMFAi9"
 //________________________________________________________________________________________________________
 global.themeemoji = process.env.GL_EMOJI || '🏮'
 //________________________________________________________________________________________________________
